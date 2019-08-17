acb57deba5ebc51a3ec944e8551c0fd3c277afd0
yyh123 <yunhaoyu@gmail.com>
2019-08-17 15:57:34 +0800

    feat:插入数据库之前数据去重

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -8,15 +8,34 @@ const db = cloud.database();
 const rp = require('request-promise')
 const URL = 'http://musicapi.xiecheng.live/personalized'
 
+const playlistCollection = db.collection('playlist')
 // 云函数入口函数
 exports.main = async (event, context) => {
+  const list = await playlistCollection.get()
+
  const playlist = await rp(URL).then((res) => {
    return JSON.parse(res).result
  })
- for(let i = 0, len = playlist.length; i < len; i++) {
-  await db.collection('playlist').add({
+
+const newData = []
+for(let i = 0,len1 = playlist.length; i < len1; i++){
+  let flag = true
+  for(let j = 0,len2 = list.data.length; j < len2; j++) {
+    if(playlist[i].id === list.data[j].id){
+      flag = false
+      break
+    }
+  }
+  if(flag) {
+    newData.push(playlist[i])
+  }
+}
+
+
+  for (let i = 0, len = newData.length; i < len; i++) {
+   await playlistCollection.add({
      data:{
-       ...playlist[i],
+       ...newData[i],
        createTime: db.serverDate(),
      }
    }).then((res)=>{
@@ -25,4 +44,5 @@ exports.main = async (event, context) => {
      console.error('插入失败')
    })
  }
+ return newData.length
 }
